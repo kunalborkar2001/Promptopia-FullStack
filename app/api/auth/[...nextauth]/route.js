2aa43056f19f1f42cee7fb0cc5e4fd6596d70f4e
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-12-23 14:44:58 +0530

    Home Page

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+
+import { connectToDb } from "@utils/database";
+import User from "@models/user";
+
+// console.log({
+//     clientId: process.env.GOOGLE_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// })
+
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    callbacks : {
+        
+        async session({ session }) {
+            const sessionUser = await User.fingOne({
+                email: session.user.email
+            })
+    
+            session.user.id = sessionUser._id_toString();
+    
+            return session 
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDb()
+    
+                // Check if a user already exists
+                const userExists = await User.findOne({
+                    email : profile.email
+                })
+                // if not, create a new user
+    
+                if(!userExists) {
+                    await User.create({
+                        email : profile.name,
+                        username : profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+    
+                return true
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+})
+
+
+export { handler as GET, handler as POST }
